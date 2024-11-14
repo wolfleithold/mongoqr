d3c04204240ea177bbac5503c80ff40460a23d50
Wolfgang Leithold <wolfleithold@gmail.com>
2024-11-14 15:32:40 -0800

    testing new APP

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,17 +1,11 @@
-// client/src/App.jsx
-import React from "react";
-import { Routes, Route } from "react-router-dom";
-import Home from "./components/Home";
-import DrinkDetails from "./components/DrinkDetails";
-import AllDrinks from "./components/AllDrinks";
+import { Outlet } from "react-router-dom";
 
 function App() {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/drinks" element={<AllDrinks />} />
-      <Route path="/drinks/:id" element={<DrinkDetails />} />
-    </Routes>
+    <div>
+      <h1>Welcome to Booze Buddy</h1>
+      <Outlet /> 
+    </div>
   );
 }
 
