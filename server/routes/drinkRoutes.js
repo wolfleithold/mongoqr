356d2f108565585f55fcf58b9fe8183e3a2ffe8a
Wolfgang Leithold <wolfleithold@gmail.com>
2024-11-15 14:49:00 -0800

    adjusted routes

--- server/routes/drinkRoutes.js
+++ server/routes/drinkRoutes.js
@@ -13,10 +13,10 @@ const router = express.Router();
 
 router.post("/", createDrink); // POST /api/drinks - Create a new drink
 router.get("/", getDrinks); // GET /api/drinks - Get all drinks
+router.get("/views", getAllDrinksWithViews); // GET /api/drinks/views - Get all drinks with views
 router.get("/:id", getDrinkById); // GET /api/drinks/:id - Get a drink by ID
 router.put("/:id", updateDrink); // PUT /api/drinks/:id - Update a drink
 router.delete("/:id", deleteDrink); // DELETE /api/drinks/:id - Delete a drink
 router.patch("/:id", patchDrink); // PATCH /api/drinks/:id - Patch a drink
-router.get("/views", getAllDrinksWithViews); // GET /api/drinks/views - Get all drinks with views
 
 module.exports = router;
