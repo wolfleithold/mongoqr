7ce60e4c40bbab2b4b8b2fdaa61331d7311df665
Wolfgang Leithold <wolfleithold@gmail.com>
2024-11-14 18:30:40 -0800

    updated vite config

--- client/vite.config.js
+++ client/vite.config.js
@@ -3,5 +3,10 @@ import react from "@vitejs/plugin-react";
 
 export default defineConfig({
   plugins: [react()],
-  base: "./", // This ensures relative paths are used in production
+  server: {
+    historyApiFallback: true, // Add fallback for local testing
+  },
+  build: {
+    outDir: "dist",
+  },
 });
